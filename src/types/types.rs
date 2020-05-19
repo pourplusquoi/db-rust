b93db80b877c12e12c0f41472014478154c29476
Yang <pourplusquoi@icloud.com>
2020-05-19 09:54:39 -0700

    Define Operation trait

--- src/types/types.rs
+++ src/types/types.rs
@@ -3,7 +3,8 @@
 
 use crate::types::value::Value;
 
-pub enum Type {
+#[derive(Clone)]
+pub enum Types<'a> {
   Boolean(i8),
   TinyInt(i8),
   SmallInt(i16),
@@ -11,30 +12,25 @@ pub enum Type {
   BigInt(i64),
   Decimal(f64),
   Timestamp(u64),
-  Varchar(Varlen),
+  Varchar(Varlen<'a>),
 }
 
-pub enum Varlen {
+#[derive(Clone)]
+pub enum Varlen<'a> {
   Owned(String),
-  Borrowed(&'static str),
+  Borrowed(&'a str),
 }
 
-pub enum CmpBool {
-  CmpTrue,
-  CmpFalse,
-  CmpNull,
-}
-
-impl Varlen {
+impl<'a> Varlen<'a> {
   pub fn len(&self) -> usize {
     match self {
-      &Varlen::Owned(ref s) => s.len(),
-      &Varlen::Borrowed(ref s) => s.len(),
+      Varlen::Owned(s) => s.len(),
+      Varlen::Borrowed(s) => s.len(),
     }
   }
 }
 
-impl Type {
+impl<'a> Types<'a> {
   pub fn data(&self) -> Option<&[u8]> {
     match self {
       Self::Varchar(varlen) => match varlen {
@@ -56,4 +52,111 @@ impl Type {
       _ => None,
     }
   }
+
+  pub fn is_coercable_from(&self, other: &Self) -> bool {
+    match self {
+      Self::Boolean(_) => true,
+      Self::TinyInt(_) | Self::SmallInt(_) |
+      Self::Integer(_) | Self::BigInt(_) |
+      Self::Decimal(_) => match other {
+        Self::TinyInt(_) | Self::SmallInt(_) |
+        Self::Integer(_) | Self::BigInt(_) |
+        Self::Decimal(_) | Self::Varchar(_) => true,
+        _ => false,
+      },
+      Self::Timestamp(_) => {
+        match other {
+          Self::Timestamp(_) | Self::Varchar(_) => true,
+          _ => false,
+        }
+      },
+      Self::Varchar(_) => true,
+    }
+  }
+
+  pub fn type_size(&self) -> usize {
+    match self {
+      Self::Boolean(_) => 1,
+      Self::TinyInt(_) => 1,
+      Self::SmallInt(_) => 2,
+      Self::Integer(_) => 4,
+      Self::BigInt(_) => 8,
+      Self::Decimal(_) => 8,
+      Self::Timestamp(_) => 8,
+      Self::Varchar(_) => 0,
+    }
+  }
+
+  pub fn type_id(&self) -> String {
+    String::from(match self {
+      Self::Boolean(_) => "BOOLEAN",
+      Self::TinyInt(_) => "TINYINT",
+      Self::SmallInt(_) => "SMALLINT",
+      Self::Integer(_) => "INTEGER",
+      Self::BigInt(_) => "BIGINT",
+      Self::Decimal(_) => "DECIMAL",
+      Self::Timestamp(_) => "TIMESTAMP",
+      Self::Varchar(_) => "VARCHAR",
+    })
+  }
+
+  pub fn boolean() -> Self {
+    Self::Boolean(0)
+  }
+
+  pub fn tinyint() -> Self {
+    Self::TinyInt(0)
+  }
+
+  pub fn smallint() -> Self {
+    Self::SmallInt(0)
+  }
+
+  pub fn integer() -> Self {
+    Self::Integer(0)
+  }
+
+  pub fn bigint() -> Self {
+    Self::BigInt(0)
+  }
+
+  pub fn decimal() -> Self {
+    Self::Decimal(0.0)
+  }
+
+  pub fn timestamp() -> Self {
+    Self::Timestamp(0)
+  }
+
+  // pub fn varchar_owned() -> Self {
+  //   Self::Varchar(Varlen::Owned(String::from("")))
+  // }
+
+  // pub fn min_val(Self) -> Self {}
+
+  // pub fn max_val(Self) -> Self {}
+}
+
+pub trait Operation : Sized {
+  fn eq(&self, other: &Self) -> Option<bool>;
+  fn ne(&self, other: &Self) -> Option<bool>;
+  fn lt(&self, other: &Self) -> Option<bool>;
+  fn le(&self, other: &Self) -> Option<bool>;
+  fn gt(&self, other: &Self) -> Option<bool>;
+  fn ge(&self, other: &Self) -> Option<bool>;
+  fn add(&self, other: &Self) -> Option<Self>;
+  fn subtract(&self, other: &Self) -> Option<Self>;
+  fn multiply(&self, other: &Self) -> Option<Self>;
+  fn divide(&self, other: &Self) -> Option<Self>;
+  fn modulo(&self, other: &Self) -> Option<Self>;
+  fn min(&self, other: &Self) -> Option<Self>;
+  fn max(&self, other: &Self) -> Option<Self>;
+  fn sqrt(&self, other: &Self) -> Option<Self>;
+  fn null(&self, other: &Self) -> Option<Self>;
+  fn is_zero(&self) -> bool;
+  fn is_inlined(&self) -> bool;
+  fn to_string(&self) -> String;
+  fn serialize_to(&self, dst: &mut [u8]);
+  fn deserialize_from(&mut self, src: &[u8]);
+  fn cast(&self, dst: &mut Self) -> bool;
 }
--- src/types/value.rs
+++ src/types/value.rs
@@ -1,38 +1,30 @@
 #![allow(dead_code)]
 
-use crate::types::types::CmpBool;
-use crate::types::types::Type;
+use crate::types::types::Operation;
+use crate::types::types::Types;
 
-pub struct Value {
-  content: Type,
+pub struct Value<'a> {
+  content: Types<'a>,
   size: Option<u32>,  // Only set iff |content| is Varchar.
 }
 
-pub fn cmp_bool(val: bool) -> CmpBool {
-  if val {
-    CmpBool::CmpTrue
-  } else {
-    CmpBool::CmpFalse
-  }
-}
-
-impl Value {
-  pub fn new(content: Type) -> Self {
+impl<'a> Value<'a> {
+  pub fn new(content: Types<'a>) -> Self {
     Value {
       size: match &content {
         // Assuming the length of string fits in u32.
-        Type::Varchar(val) => Some(val.len() as u32),
+        Types::Varchar(val) => Some(val.len() as u32),
         _ => None,
       },
       content: content,
     }
   }
 
-  pub fn borrow(&self) -> &Type {
+  pub fn borrow(&self) -> &'a Types {
     &self.content
   }
 
-  pub fn borrow_mut(&mut self) -> &mut Type {
+  pub fn borrow_mut(&mut self) -> &'a mut Types {
     &mut self.content
   }
 
@@ -47,4 +39,28 @@ impl Value {
   }
 
   // pub fn data() -> {}
+}
+
+impl<'a> Operation for Value<'a> {
+  fn eq(&self, other: &Self) -> Option<bool> {None}
+  fn ne(&self, other: &Self) -> Option<bool> {None}
+  fn lt(&self, other: &Self) -> Option<bool> {None}
+  fn le(&self, other: &Self) -> Option<bool> {None}
+  fn gt(&self, other: &Self) -> Option<bool> {None}
+  fn ge(&self, other: &Self) -> Option<bool> {None}
+  fn add(&self, other: &Self) -> Option<Self> {None}
+  fn subtract(&self, other: &Self) -> Option<Self> {None}
+  fn multiply(&self, other: &Self) -> Option<Self> {None}
+  fn divide(&self, other: &Self) -> Option<Self> {None}
+  fn modulo(&self, other: &Self) -> Option<Self> {None}
+  fn min(&self, other: &Self) -> Option<Self> {None}
+  fn max(&self, other: &Self) -> Option<Self> {None}
+  fn sqrt(&self, other: &Self) -> Option<Self> {None}
+  fn null(&self, other: &Self) -> Option<Self> {None}
+  fn is_zero(&self) -> bool {false}
+  fn is_inlined(&self) -> bool {false}
+  fn to_string(&self) -> String {String::from("")}
+  fn serialize_to(&self, dst: &mut [u8]) {}
+  fn deserialize_from(&mut self, src: &[u8]) {}
+  fn cast(&self, dst: &mut Self) -> bool {false}
 }
