20c6a45133081cdb96f78e3ff04923a73b26a33b
Yang <pourplusquoi@icloud.com>
2020-05-21 04:49:50 -0700

    Update bitmap.rs

--- src/disk/bitmap.rs
+++ src/disk/bitmap.rs
@@ -1,9 +1,8 @@
-#![allow(dead_code)]
-
 use crate::common::config::CHECKSUM_SIZE;
 use crate::disk::disk_manager::read;
 use crate::disk::disk_manager::write;
 use crate::logging::error_logging::ErrorLogging;
+use std::collections::BTreeSet;
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::Seek;
@@ -12,15 +11,16 @@ use std::ops::Drop;
 
 // Using `u8` as word, which has 8 bytes.
 const BITS_PER_WORD: usize = 8;
+const FULL_WORD: u8 = 255;
 
 pub struct Bitmap {
   file: File,
   cache: Vec<u8>,
+  free: BTreeSet<usize>,
 }
 
 impl Drop for Bitmap {
   fn drop(&mut self) {
-    self.truncate();
     // Unable to handle errors on destruction.
     self.sync().log();
   }
@@ -36,6 +36,7 @@ impl Bitmap {
           .create(true)
           .open(path)?,
       cache: Vec::new(),
+      free: BTreeSet::new(),
     }).and_then(|mut bitmap| {
       bitmap.init()?;
       Ok(bitmap)
@@ -47,9 +48,10 @@ impl Bitmap {
   }
 
   pub fn find(&self) -> usize {
-    for (word_idx, &word) in self.data().iter().enumerate() {
-      if word < 255 {
-        let bit = {
+    match self.free.iter().nth(0) {
+      Some(&word_idx) => {
+        let word = self.data()[word_idx];
+        let bit_idx = {
           if word & 128 == 0 { 0 }
           else if word & 64 == 0 { 1 }
           else if word & 32 == 0 { 2 }
@@ -59,10 +61,10 @@ impl Bitmap {
           else if word & 2 == 0 { 6 }
           else { 7 }
         };
-        return word_idx * BITS_PER_WORD + bit;
-      }
+        word_idx * BITS_PER_WORD + bit_idx
+      },
+      None => self.data().len() * BITS_PER_WORD,
     }
-    self.data().len() * BITS_PER_WORD
   }
 
   pub fn set_bit(&mut self, idx: usize, bit: bool) {
@@ -74,6 +76,12 @@ impl Bitmap {
       self.data_mut()[word_idx] |= mask;
     } else {
       self.data_mut()[word_idx] &= !mask;
+    }
+    // Update the free set.
+    if self.data()[word_idx] < FULL_WORD {
+      self.free.insert(word_idx);
+    } else {
+      self.free.remove(&word_idx);
     }
   }
 
@@ -87,9 +95,9 @@ impl Bitmap {
     self.data()[word_idx] & mask > 0
   }
 
-  // Truncates and persists to disk.
+  // Compacts and persists to disk.
   pub fn sync(&mut self) -> std::io::Result<()> {
-    self.truncate();
+    self.compact();
     let size = self.cache.len();
     self.file.set_len(size as u64)?;
     self.file.seek(SeekFrom::Start(0))?;
@@ -98,10 +106,11 @@ impl Bitmap {
   }
 
   // Truncates the tailing zeros.
-  pub fn truncate(&mut self) {
+  pub fn compact(&mut self) {
     while let Some(&word) = self.cache.last() {
       if word == 0 && self.data().len() > 0 {
         self.cache.pop();
+        self.free.remove(&self.data().len());
       } else {
         break;
       }
@@ -115,6 +124,13 @@ impl Bitmap {
       read(&mut self.file, self.cache.as_mut(), size)?;
     } else {
       self.cache = vec![0; CHECKSUM_SIZE];
+    }
+    // Initialize the free set.
+    let iter = self.cache.iter().skip(CHECKSUM_SIZE);
+    for (word_idx, &word) in iter.enumerate() {
+      if word < FULL_WORD {
+        self.free.insert(word_idx);
+      }
     }
     Ok(())
   }
@@ -192,17 +208,20 @@ mod tests {
 
     bitmap.set_bit(80, false);
     assert_eq!(80, bitmap.find());
+    bitmap.set_bit(80, true);
+    assert_eq!(128, bitmap.find());
 
     for i in 64..128 {
       bitmap.set_bit(i, false);
-      assert_eq!(64, bitmap.find());
+      assert!(bitmap.find() >= 64);
+      assert!(bitmap.find() <= i, "{}, {}", bitmap.find(), i);
     }
-    bitmap.truncate();
+    bitmap.compact();
     assert_eq!(8, bitmap.len());
   }
 
   #[test]
-  fn len_and_truncate() {
+  fn len_and_compact() {
     let path = "/tmp/testfile.bitmap.3.db";
 
     // Test file deleter with RAII.
@@ -219,12 +238,12 @@ mod tests {
 
     bitmap.set_bit(4321, false);
     assert_eq!(541, bitmap.len());
-    bitmap.truncate();
+    bitmap.compact();
     assert_eq!(155, bitmap.len());
 
     bitmap.set_bit(1234, false);
     assert_eq!(155, bitmap.len());
-    bitmap.truncate();
+    bitmap.compact();
     assert_eq!(0, bitmap.len());
   }
 
@@ -247,7 +266,7 @@ mod tests {
 
       bitmap.set_bit(4321, false);
       assert_eq!(541, bitmap.len());
-    }  // Drops bitmap: truncates and persist to disk.
+    }  // Drops bitmap: compacts and persist to disk.
 
     {
       let result = Bitmap::new(&path);
@@ -260,7 +279,7 @@ mod tests {
 
       bitmap.set_bit(1234, false);
       assert_eq!(155, bitmap.len());
-    }  // Drops bitmap: truncates and persist to disk.
+    }  // Drops bitmap: compacts and persist to disk.
 
     {
       let result = Bitmap::new(&path);
